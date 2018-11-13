71105530aa182c0d6c04c341ee04aa51ad18ac6a
Conaill Hyndman <chyndman@deloitte.co.uk>
2018-11-13 11:53:39 +0000

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    var $header = $("header");
+    var $sticky = $header.before($header.clone().addClass("sticky"));
+    $(window).on("scroll", function(){
+        var scrollFromTop = $(window).scrollTop();
+        $("body").toggleClass("scroll", (scrollFromTop >350));
+    });
+
+    //masonry
+    $(".grid").masonry({
+        //options
+        itemSelector: ".grid-item",
+        columnwidth: 120,
+        fitWidth: true,
+        gutter: 25
+    });
+
+    //Slick Slider
+    $(".slider").slick({
+        arrows: true,
+        prevArrow: "<button type='button' class='slick-prev'>Previous</button>",
+        nextArrow: "<button type='button' class='slick-next'>Next</button>"
+    });
+
+});
