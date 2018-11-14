e93c7950754c55b80bd42273979e0b98cea6c766
Conaill Hyndman <chyndman@deloitte.co.uk>
2018-11-14 12:57:03 +0000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -33,6 +33,20 @@ $(document).ready(function(){
         slidesToShow: 3,
         prevArrow: "<button type='button' class='slick-prev'>Previous</button>",
         nextArrow: "<button type='button' class='slick-next'>Next</button>"
+        responsive: [
+            {
+                breakpoint: 990,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
     //responsive menu start
