52fbf9716738786f86fa64c9c38ec0be768d8c88
Conaill Hyndman <chyndman@deloitte.co.uk>
2018-11-20 14:28:46 +0000

    formatting js

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var $header = $("header");
     var $sticky = $header.before($header.clone().addClass("sticky"));
-    $(window).on("scroll", function(){
+    $(window).on("scroll", function () {
         var scrollFromTop = $(window).scrollTop();
         $("body").toggleClass("scroll", (scrollFromTop > 350));
     });
 
     //smooth scroll
-    $(".menu li a[href^='#']").on("click", function(e){
+    $(".menu li a[href^='#']").on("click", function (e) {
         e.preventDefault();
         var target = $(this.hash);
         if (target.length) {
             $('html, body').stop().animate({
-                scrollTop: target.offset().top -60
+                scrollTop: target.offset().top - 60
             }, 1000);
         }
     });
@@ -54,15 +54,15 @@ $(document).ready(function(){
     var menuTrigger = $('.js-menu-trigger');
     var mainOverlay = $('.js-main-overlay');
 
-    menuTrigger.on('click', function(){
+    menuTrigger.on('click', function () {
         body.addClass('menu-is-active');
     });
-    
-    mainOverlay.on('click', function(){
+
+    mainOverlay.on('click', function () {
         body.removeClass('menu-is-active');
     });
 
-    $('.menu li a').on('click', function(){
+    $('.menu li a').on('click', function () {
         $('body').removeClass("menu-is-active");
     });
 });
